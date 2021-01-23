727ebd742853760c167377c7ba827e14670ea5c1
AllaYeroshchenko <yeroshchenko@gmail.com>
2021-01-23 15:53:51 -0800

    leetcode problems

---
+++ leetcode/Binary_Search.py
@@ -0,0 +1,14 @@
+# 704. Binary Search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            center = left + (right-left)//2
+            if nums[center] == target:
+                return center
+            elif nums[center] < target:
+                left = center+1
+            else:
+                right = center-1
+        return -1        
