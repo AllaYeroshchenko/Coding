91ead7f90ab96f6902cb6ca22a894f9b34d64626
AllaYeroshchenko <yeroshchenko@gmail.com>
2021-02-15 15:08:52 -0800

    coding interview

--- leetcode/Can_Place_Flowers.py
+++ leetcode/Can_Place_Flowers.py
@@ -2,7 +2,8 @@
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        flowerbed = [0] + flowerbed + [0]
+        flowerbed.append(0) 
+        flowerbed.insert(0, 0)
         i=1
         while i<len(flowerbed)-1:
             if flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
